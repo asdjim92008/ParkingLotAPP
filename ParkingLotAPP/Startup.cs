a97d3c671b56bb1a1e23afee9362101b0fb0caf8
Liwen <evermore210007@gmail.com>
2022-05-24 14:48:50 +0800

    20220524_LoginVerify

--- ParkingLotAPP/Startup.cs
+++ ParkingLotAPP/Startup.cs
@@ -50,7 +50,7 @@ namespace ParkingLotAPP
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
