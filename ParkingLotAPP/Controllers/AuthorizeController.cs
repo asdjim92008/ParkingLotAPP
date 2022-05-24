1557924eab276738838770c52edd7118d4583285
a0979269549 <a0979269549@gmail.com>
2022-05-24 13:25:16 +0800

    session測試

--- ParkingLotAPP/Controllers/AuthorizeController.cs
+++ ParkingLotAPP/Controllers/AuthorizeController.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ParkingLotAPP.HELP;
+using static ParkingLotAPP.Models.DataModel;
 
 namespace ParkingLotAPP.Controllers
 {
@@ -17,17 +19,39 @@ namespace ParkingLotAPP.Controllers
         [HttpPost]
         public ActionResult Login(string Account,string Password)
         {
-            DataModel.Response response;
-            var manager = manager_SQL.Login(Account, Password);
-            if (manager!=null)
+            Response response;
+            var session = HttpContext.Session.GetObjectFromJson<SessionState>("mySession");
+
+            //已登入的回傳
+            if (session!= null)
             {
-                response = new DataModel.Response { Code = "200", ErrMsg = "", Data = manager };
+                response = new Response { Code = "201", ErrMsg = "已登入", Data = session };
             }
             else
             {
-                response = new DataModel.Response { Code = "400", ErrMsg = "帳號密碼錯誤", Data = manager };
+                //建立儲存session的物件
+                SessionState sessionState = new SessionState();
+                HttpContext.Session.SetObjectAsJson("mySession", sessionState);
+                session = HttpContext.Session.GetObjectFromJson<SessionState>("mySession");
+
+                var manager = manager_SQL.Login(Account, Password);
+
+                //登入成功
+                if (manager != null)
+                {
+                    response = new Response { Code = "200", ErrMsg = "", Data = manager };
+                    session.Account = manager.Account;
+                    session.UserName = manager.UserName;
+                    HttpContext.Session.SetObjectAsJson("mySession", session);
+                }
+                //登入失敗
+                else
+                {
+                    response = new Response { Code = "400", ErrMsg = "帳號密碼錯誤", Data = manager };
+                }
             }
-            
+
+
             return Json(response);
         }
     }
--- ParkingLotAPP/Startup.cs
+++ ParkingLotAPP/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,6 +25,8 @@ namespace ParkingLotAPP
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +44,7 @@ namespace ParkingLotAPP
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseRouting();
 
             app.UseAuthorization();
