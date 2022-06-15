c1b585e0a4bbb4373aedf0983057f762daec9541
Liwen <evermore210007@gmail.com>
2022-06-16 07:50:11 +0800

    (遠端)畫面精進合併

---
+++ ParkingLotAPP/Controllers/ManualaddController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ParkingLotAPP.HELP;
+using ParkingLotAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using static ParkingLotAPP.Models.DataModel;
+
+namespace ParkingLotAPP.Controllers
+{
+    public class ManualaddController : Controller
+    {
+        #region 手動新增
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
