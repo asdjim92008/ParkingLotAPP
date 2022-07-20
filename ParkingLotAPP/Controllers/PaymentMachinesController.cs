55f7a97bafeb06278e778e78d1fd39028f761c4f
Liwen <evermore210007@gmail.com>
2022-07-20 16:44:11 +0800

    前端: 收費機訊息layout

---
+++ ParkingLotAPP/Controllers/PaymentMachinesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ParkingLotAPP.HELP;
+using ParkingLotAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using static ParkingLotAPP.Models.DataModel;
+
+namespace ParkingLotAPP.Controllers
+{
+    public class PaymentMachinesController : Controller
+    {
+        #region 繳費機
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
